4182c1f841511d47f529d47ed9ea69c71a008dcd
tamarneter <70896524+tamarneter@users.noreply.github.com>
2021-05-06 13:42:04 +0300

    Add files via upload

--- DataGiladTamar.m
+++ DataGiladTamar.m
@@ -13,18 +13,24 @@ TstartTamar = duration('16:08:15.381','InputFormat',infmt,'Format',outfmt);
 i = 0; %0 for ei 1 for delta t
 if i == 0
     %% episode identification
+    % interp1
+    DataGilad(:,1) = DataGilad(:,1) - DataGilad(1,1);
+    DataGiladInterp = interp1(DataGilad(:,1),DataGilad(:,2:end),0:0.002:round(DataGilad(end-1,1),2));
+    DataTamarInterp = interp1(DataTamar(:,2),DataTamar(:,3:end),0:0.002:round(DataGilad(end-1,1),2));
+    DataGilad = [(0:0.002:round(DataGilad(end,1),2))',DataGiladInterp];
+    DataTamar = [(0:0.002:round(DataGilad(end,1),2))',DataTamarInterp];
     % finding the index of the episode in the data
     startGidx = find(abs(DataGilad(:,2)) >= 0.7,1,'first');
-    startTidx = find(abs(DataTamar(:,5)-DataTamar(1,5)) >= 0.002,1,'first');
+    startTidx = find(abs(DataTamar(:,4)-DataTamar(1,4)) >= 0.002,1,'first');
     % the start time in [s] of the episode
     TstartG = DataGilad(startGidx,1);
-    TstartT = DataTamar(startTidx,2);
+    TstartT = DataTamar(startTidx,1);
     dt = TstartT - TstartG;
     
     if dt > 0
-        [d, id] = min(abs(DataTamar(:,2) - dt));
+        [d, id] = min(abs(DataTamar(:,1) - dt));
         DataTamar = DataTamar(id:end,:);
-        DataTamar(:,2) = DataTamar(:,2) - DataTamar(1,2);
+        DataTamar(:,1) = DataTamar(:,1) - DataTamar(1,1);
     else
         [d, id] = min(abs(DataGilad(:,1) - dt));
         DataGilad = DataGilad(id:end,:);
@@ -40,17 +46,15 @@ if i == 0
     end
     
     TstartG_new = DataGilad(find(abs(DataGilad(:,2)) >= 0.7,1,'first'),1);
-%     DataTamarnew(:,1) = resample(DataTamar(:,2),3,1);
-%     DataTamarnew(:,2:64) = resample(DataTamar(:,3:end),3,1);
-    TstartT_new = DataTamar(find(abs(DataTamar(:,5) - DataTamar(1,5)) >= 0.002,1,'first'),2);
+    TstartT_new = DataTamar(find(abs(DataTamar(:,4) - DataTamar(1,4)) >= 0.002,1,'first'),1);
     
     Dt = abs(TstartG_new -TstartT_new)
     mat = nan(r,EIcolG+EIcolT-1);
     mat(1:EIrowG,1:EIcolG) = DataGilad;
     mat(1:EIrowT,EIcolG+1:EIcolT+EIcolG-1) =  DataTamar(:,2:end);
     %tests
-    hold on;
-    plot(DataTamar(:,2),DataTamar(:,5)); plot(DataGilad(:,1),DataGilad(:,2));
+%     hold on;
+%     plot(DataTamar(:,2),DataTamar(:,5)); plot(DataGilad(:,1),DataGilad(:,2));
 else
 %% delta t option - 0.4[s] inaccuracy :(
     deltaT = TstartTamar - (TstartGilad + seconds(DataGilad(1:1)));
@@ -81,9 +85,7 @@ else
         mat = nan(r,cG+cT-1);
         mat(1:rG,1:cG) = DataGilad_new;
         mat(1:rT,cG+1:cT+cG-1) =  DataTamar_new(:,2:end);
-
-       
-    %    
+        
     %    figure;
     %    plot(mat(:,8),mat(:,11:3:end)-mat(1,11:3:end)); xlabel('time [s]'); ylabel('Z displacement [mm]');
     %    %     end
@@ -97,4 +99,26 @@ subplot(324); plot(mat(:,1),mat(:,5)); xlabel('time [s]'); ylabel('g_{x}');
 subplot(325); plot(mat(:,1),mat(:,6)); xlabel('time [s]'); ylabel('g_{y}');
 subplot(326); plot(mat(:,1),mat(:,7)); xlabel('time [s]'); ylabel('g_{z}');
 figure;
-plot(mat(:,8),mat(:,11:3:end));
+plot(mat(:,1),mat(:,8:3:end));
+% fft
+for i = [1:10]
+    y(:,i) = mat(TstartT_new:end,5+3*i);
+    y1_norm = (y(:,i)-y(1,i))./y(1,i);
+    y1_norm(isnan(y1_norm)) = [];
+    x = mat(:,8);
+    Fs = 500;
+    Y = fft(y1_norm);
+    T = 1/Fs;
+    L = length(y1_norm);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    
+    semilogy(f,P1)
+    hold on
+    title('Single-Sided Amplitude Spectrum of X(t)')
+    xlabel('f (Hz)')
+    ylabel('|P1(f)|')
+    xlim([0,40]);
+end
