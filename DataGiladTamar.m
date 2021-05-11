b10a7bbf35d3b4a2828b2eed3c3beecd8b7fd0c7
tamarneter <70896524+tamarneter@users.noreply.github.com>
2021-05-11 15:55:54 +0300

    Add files via upload

--- DataGiladTamar.m
+++ DataGiladTamar.m
@@ -2,30 +2,31 @@ clear;
 close;
 clc;
 %function [mat] = DataGiladTamar(DataGilad,DataTamar,TstartGilad,TstartTamar)
-outfmt = 'hh:mm:ss.SSS';
-infmt = 'hh:mm:ss.SSS';
 %%
 newfilename = "test2";
 mkdir (["../data/"+newfilename])
-filename = 0; % 1 for choosing the file interactively
+filename = 1; % 1 for choosing the file interactively
+folder = 'C:\Users\tamar\OneDrive - Technion\Documents\technion\semester 6\mabada\21April2021\data';
 if filename == 0
-    filenameG = '../data/28-4-2021_17-05-45-652.txt';
-    filenameT = '../data/Take 2021-05-05 04.36.50 PM_001.csv';
+    filenameG = '../data/02-5-2021_10-43-16-137.txt';
+    filenameT = '../data/11May2021 (1).csv';
 else
-    filenameG = uigetfile('../data/*.txt');
-    filenameT = uigetfile('../data/*.csv');
+    filenameG = fullfile(folder, uigetfile('../data/*.txt'));
+    filenameT = fullfile(folder, uigetfile('../data/*.csv'));
+%     filenameG = uigetfile('../data/*.txt');
+%     filenameT = uigetfile('../data/*.csv');
     % adding a path: [filename, path] = uigetfile('../data/*.txt'); 
 end
 
 DataGilad = readmatrix(filenameG); %28-4-2021_16-08-16-471.txt
-TstartGilad = duration('16:08:16.471','InputFormat',infmt,'Format',outfmt);
+% TstartGilad = duration('16:08:16.471','InputFormat',infmt,'Format',outfmt);
 DataTamar = table2array(readtable(filenameT)); %Take 2021-04-28 04.08.15 PM.csv
 DataTamar = DataTamar(:,(~isnan(DataTamar(1,:))));   % for nan - columns
-TstartTamar = duration('16:08:15.381','InputFormat',infmt,'Format',outfmt);
+% TstartTamar = duration('16:08:15.381','InputFormat',infmt,'Format',outfmt);
 
-i = 0; %0 for ei 1 for delta t
-if i == 0
     %% episode identification
+    tolT = 0.002;
+    tolG = 0.7;
     % interp1
     DataGilad(:,1) = DataGilad(:,1) - DataGilad(1,1);
     DataGiladInterp = interp1(DataGilad(:,1),DataGilad(:,2:end),0:0.002:round(DataGilad(end-1,1),2));
@@ -33,8 +34,8 @@ if i == 0
     DataGilad = [(0:0.002:round(DataGilad(end,1),2))',DataGiladInterp];
     DataTamar = [(0:0.002:round(DataGilad(end,1),2))',DataTamarInterp];
     % finding the index of the episode in the data
-    startGidx = find(abs(DataGilad(:,2)) >= 0.7,1,'first');
-    startTidx = find(abs(DataTamar(:,4)-DataTamar(1,4)) >= 0.002,1,'first');
+    startGidx = find(abs(DataGilad(:,2)) >= tolG,1,'first');
+    startTidx = find(abs(DataTamar(:,2)-DataTamar(1,2)) >= tolT,1,'first');
     % the start time in [s] of the episode
     TstartG = DataGilad(startGidx,1);
     TstartT = DataTamar(startTidx,1);
@@ -59,51 +60,14 @@ if i == 0
         r = EIrowT;
     end
     
-    TstartG_new = DataGilad(find(abs(DataGilad(:,2)) >= 0.7,1,'first'),1);
-    TstartT_new = DataTamar(find(abs(DataTamar(:,4) - DataTamar(1,4)) >= 0.002,1,'first'),1);
+    TstartG_new = DataGilad(find(abs(DataGilad(:,2)) >= tolG,1,'first'),1);
+    TstartT_new = DataTamar(find(abs(DataTamar(:,4) - DataTamar(1,4)) >= tolT,1,'first'),1);
     
     Dt = abs(TstartG_new -TstartT_new)
     mat = nan(r,EIcolG+EIcolT-1);
     mat(1:EIrowG,1:EIcolG) = DataGilad;
     mat(1:EIrowT,EIcolG+1:EIcolT+EIcolG-1) =  DataTamar(:,2:end);
-    %tests
-%     hold on;
-%     plot(DataTamar(:,2),DataTamar(:,5)); plot(DataGilad(:,1),DataGilad(:,2));
-else
-%% delta t option - 0.4[s] inaccuracy :(
-    deltaT = TstartTamar - (TstartGilad + seconds(DataGilad(1:1)));
-        if deltaT > 0
-            StartIdxG = find(DataGilad(:,1) >= seconds(abs(deltaT)),1,'first');
-            DataGilad_new = DataGilad(StartIdxG:end,:);
-            DataGilad_new(:,1) = DataGilad_new(:,1) - DataGilad_new(1,1);
-            DataTamar_new = DataTamar;
-            [rG, cG] = size(DataGilad_new);
-            [rT, cT] = size(DataTamar_new);
-        else
-            StartIdxT = find(DataTamar(:,2) >= seconds(abs(deltaT)),1,'first');
-            DataTamar(280,3) = nan;
-            DataTamar_new = DataTamar(StartIdxT:end,:);
-            DataTamar_new(:,2) = DataTamar_new(:,2) - DataTamar_new(1,2);
-            DataGilad_new = DataGilad;
-            DataGilad_new(:,1) = DataGilad(:,1) - DataGilad(1,1);
-            [rT, cT] = size(DataTamar_new);
-            [rG, cG] = size(DataGilad_new);
-        end
 
-        if [rG > rT]
-            r = rG;
-        else
-            r = rT;
-        end
-
-        mat = nan(r,cG+cT-1);
-        mat(1:rG,1:cG) = DataGilad_new;
-        mat(1:rT,cG+1:cT+cG-1) =  DataTamar_new(:,2:end);
-        
-    %    figure;
-    %    plot(mat(:,8),mat(:,11:3:end)-mat(1,11:3:end)); xlabel('time [s]'); ylabel('Z displacement [mm]');
-    %    %     end
-end
 %% figures
 figure;
 subplot(321); plot(mat(:,1),mat(:,2)); xlabel('time [s]'); ylabel('a_{x}');
@@ -115,6 +79,11 @@ subplot(326); plot(mat(:,1),mat(:,7)); xlabel('time [s]'); ylabel('g_{z}');
 figure;
 plot(mat(:,1),mat(:,8:3:end));
     % fft
+if isempty(TstartT_new)
+    TstartT_new = 1;
+end
+% TstartT_new = TstartT_new*(~isempty(TstartT_new)) + 1;        
+figure;
 for i = [1:10]
     y(:,i) = mat(TstartT_new:end,5+3*i);
     y1_norm = (y(:,i)-y(1,i))./y(1,i);
